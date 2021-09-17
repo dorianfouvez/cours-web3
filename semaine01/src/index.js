1e50ae7ac9daec77a3debcbd19e25a7cd4531bb9
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-09-17 10:31:13 +0200

    D

--- semaine01/src/index.js
+++ semaine01/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import App from './components/App/App'
+import App from './components/App/App' // avec le chemin absolu => 'components/App/App'
 
 ReactDOM.render(
   <App />, 
