ba5edd11d9c9d03cf286e366310e5a960168d057
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-10-08 09:35:06 +0200

    A & B

---
+++ semaine04/src/components/App/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import Person from '../Person/Person'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if(persons.find(person => person.name === newName)){
+      window.alert(`${newName} is already added to phonebook`);
+      console.log("Is include 1");
+      return;
+    }
+
+    const personObject = {
+      name: newName,
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <Person key={person.name} person={person} /> )}
+    </div>
+  )
+}
+
+export default App
