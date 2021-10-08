2b29a5494f900dad0cd4c181692ef077337742c1
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-10-08 11:13:19 +0200

    C - 2.11

--- semaine04/src/components/App/App.js
+++ semaine04/src/components/App/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from '../Filter/Filter'
 import PersonForm from '../PersonForm/PersonForm'
 import Persons from '../Persons/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setNewFilter ] = useState('')
@@ -49,6 +45,14 @@ const App = () => {
   const inputs = [ { label: 'name', value: newName, onChange: handleNameChange }, 
     { label: 'number', value: newNumber, onChange: handleNumberChange }
   ]
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
