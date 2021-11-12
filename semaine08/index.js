4100caee7d58fba860d389c566943e77f0e1fa28
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-11-12 11:20:43 +0100

    some refactor

--- semaine08/index.js
+++ semaine08/index.js
@@ -8,15 +8,15 @@ morgan.token('body', function getBody (req) {
     return JSON.stringify(req.body);
 });
 
-morgan.token('x-user-email', function getBody (req) {
-    return JSON.stringify(req.currentUser);
-});
-
 // Middleware   =>  https://expressjs.com/fr/guide/using-middleware.html
 app.use(function (req, res, next) {
     const email = req.header("X-USER-EMAIL");
     if(email) req.currentUser = { email };
     next();
+});
+
+morgan.token('x-user-email', function getEmail (req) {
+    return JSON.stringify(req.currentUser);
 });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body :x-user-email')); // tiny =>     :method :url :status :res[content-length] - :response-time ms
