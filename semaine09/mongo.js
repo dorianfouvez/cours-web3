f6198d9e5867f3b78d5f50fefc195d0bb6067873
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-11-19 09:46:42 +0100

    3.12

--- semaine09/mongo.js
+++ semaine09/mongo.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>');
-  process.exit(1);
+if (process.argv.length < 3 || process.argv.length > 5) {
+    console.log(`Please provide the password as an argument: 
+    \n\tnode mongo.js <password>
+    \n\tor
+    \n\tnode mongo.js <password> <name> <number>`);
+    process.exit(1);
 }
 
 const password = process.argv[2];
+let name;
+let number;
+if(process.argv.length > 3){
+    name = process.argv[3];
+    number = process.argv[4];
+}
 
 const url =
   `mongodb+srv://react_courses:${password}@cluster0.ezt7n.mongodb.net/phonebook-app?retryWrites=true`;
@@ -18,15 +27,26 @@ const personSchema = new mongoose.Schema({
   number: String,
 });
 
-const Note = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema);
 
-const note = new Note({
-  id: 1,
-  name: "Arto Hellas",
-  number: "040-123456",
-});
-
-note.save().then(result => {
-  console.log('person saved!');
-  mongoose.connection.close();
-});
+if(process.argv.length > 3){
+    const person = new Person({
+      id: 2,
+      name: name,
+      number: number,
+    });
+    
+    person.save().then(result => {
+      console.log(`added ${name} with number ${number} to phonebook`);
+      mongoose.connection.close();
+    });
+} else {
+    Person.find({})
+    .then(result => {
+        console.log("Phonebook:");
+        result.forEach(person => {
+          console.log(person);
+        });
+        mongoose.connection.close();
+    });
+}
