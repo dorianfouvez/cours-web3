63b3813d823599c091ff8deebcd077c5c4237863
Dorian Fouvez <dorian.fouvez@student.vinci.be>
2021-11-19 09:50:33 +0100

    3.12 refactor for message

--- semaine09/mongo.js
+++ semaine09/mongo.js
@@ -45,7 +45,7 @@ if(process.argv.length > 3){
     .then(result => {
         console.log("Phonebook:");
         result.forEach(person => {
-          console.log(person);
+          console.log(person.name + "\t" + person.number);
         });
         mongoose.connection.close();
     });
